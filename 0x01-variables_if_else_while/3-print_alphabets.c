1a52e41b36e0931b1730ba5b2f83249d03a98781
emkarrar <eman.a.karrar@outlook.com>
2023-04-21 16:16:56 -0700

    Alphas

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	int s = 97;
+	int o = 65;
+
+	while (s <= 122)
+		putchar(s);
+		s++;
+	while (o <= 90)
+		putchar(o);
+		o++;
+	putchar('\n');
+	return (0);
+}
