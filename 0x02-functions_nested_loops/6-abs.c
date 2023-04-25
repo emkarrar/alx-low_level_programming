a924807f755f04778ae62bccd75b9dbf459b2e6e
emkarrar <eman.a.karrar@outlook.com>
2023-04-25 16:22:45 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - computes abs value
+ * @i: integer
+ * Return: abs value
+ */
+int _abs(int i)
+{
+
+
+	if (i < 0)
+	{
+		return (i * -1);
+	}
+	else
+	{
+		return (i);
+	}
+	_putchar('\n');
+}
