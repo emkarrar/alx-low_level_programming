a32232c2720fe6826dcd4ad4efa3e57b9b13f999
emkarrar <eman.a.karrar@outlook.com>
2023-04-25 14:06:59 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets
+ * Return: null
+ */
+void print_alphabet(void)
+{
+	int a = 0;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
+
