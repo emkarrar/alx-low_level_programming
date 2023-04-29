c64a639bd1924fce4704b179ee6d2354b85c5cfc
emkarrar <eman.a.karrar@outlook.com>
2023-04-28 19:24:41 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - prints numbers with fizzbuzz test
- * @n: nums
  * Return: 0
  */
 
-int main(int n)
+int main(void)
 {
+	int n;
+
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 15 != 0)
@@ -26,5 +27,6 @@ int main(int n)
 		printf("%d ", n);
 		}
 	}
+	printf("\n");
 	return (0);
 }
