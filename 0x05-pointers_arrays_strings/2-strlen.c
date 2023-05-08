26278c2e0b1abc703979321a143cf0325dba6212
emkarrar <eman.a.karrar@outlook.com>
2023-05-08 14:41:43 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ * @s: &s
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int z;
+
+	z = sizeof(s);
+	_putchar(z);
+}
